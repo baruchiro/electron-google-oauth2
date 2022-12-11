53a638afa5e031402731022b68f3ec543e654bbd
Baruch Odem <baruchiro@gmail.com>
2022-12-11 18:10:14 +0200

    configure semantic-release

---
+++ release.config.js
@@ -0,0 +1,41 @@
+const conventionalCommits = {
+    preset: 'eslint'
+}
+
+module.exports = {
+    release: {
+        branches: ["master"]
+    },
+    plugins: [
+        [
+            '@semantic-release/commit-analyzer',
+            conventionalCommits
+        ],
+        [
+            '@semantic-release/release-notes-generator',
+            conventionalCommits
+        ],
+        [
+            "@semantic-release/changelog",
+            {
+                "changelogFile": "CHANGELOG.md",
+                "changelogTitle": "# Changelog",
+            }
+        ],
+        [
+            "@semantic-release/git",
+            {
+                "assets": ["CHANGELOG.md"],
+                "message": "${nextRelease.version} CHANGELOG [skip ci]\n\n${nextRelease.notes}"
+            }
+        ],
+        '@semantic-release/npm',
+        '@semantic-release/github',
+        [
+            "@semantic-release/exec",
+            {
+                "successCmd": "echo \"SEMVER_VERSION=${nextRelease.version}\" > $GITHUB_ENV"
+            }
+        ]
+    ]
+}
